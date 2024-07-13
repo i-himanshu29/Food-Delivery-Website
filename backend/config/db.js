19e2d7c0c8d0b7841210b1d111a837ec0de6a3d7
Himanshu Maurya <mauryahimanshu979@gmail.com>
2024-07-13 07:51:17 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+export const connectDB = async ()=>{
+    await mongoose.connect('mongodb+srv://mauryahimanshu979:XXDCvOJi0V2NzkWd@cluster0.pz64k14.mongodb.net/FoodyDB').then(()=>console.log("DataBase Connected"));
+
+}
